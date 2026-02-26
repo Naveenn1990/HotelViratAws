4c0ecc4f674d2ec9e9ecc94e2481ae96e2a7fbad
Parnets19 <parnetstech19@gmail.com>
2026-02-26 16:02:45 +0530

    aws bill

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.use(limiter);
 //           "data:",
 //           "http://localhost:3000",
 //           "http://localhost:5173",
-//           "https://hotelvirat.com",
+//           "http://localhost:9000",
 //           "https://hotelvirat.s3.amazonaws.com"
 //         ],
 //       },
