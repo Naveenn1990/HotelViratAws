125246cb17254a30c0397eace7018cd4d2e1431d
Parnets19 <parnetstech19@gmail.com>
2026-03-11 23:10:46 +0530

    done counter

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use(cors({
 //           "data:",
 //           "http://localhost:3000",
 //           "http://localhost:5173",
-//           "http://localhost:9000",
+//           "https://hotelvirat.com",
 //           "https://hotelvirat.s3.amazonaws.com"
 //         ],
 //       },
